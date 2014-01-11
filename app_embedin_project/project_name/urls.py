202a3b987d836af12139cae4f900e748f8b96152
yanwenxu <yanwx@knownsec.com>
2014-01-11 10:59:11 +0800

    fixbug

---
+++ app_embedin_project/project_name/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'inreal.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )
